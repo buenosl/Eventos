7ff9b7693c1553d90058f1e16b4a411367e7e4de
Sandro <sandrobueno@gmail.com>
2022-01-31 11:21:04 -0300

    Sitema Eventos

---
+++ evento.js
@@ -0,0 +1,43 @@
+// Data Evento
+let dataEvento = 05/02/2022
+if (dataEvento > 06/02/2020 ) {
+    console.log ("Data do evento inferior, não permite cadadastro ")
+} else {
+    console.log  ("Permite cadastro")
+} 
+
+let idade = 18
+if (idade >= 18) {
+    console.log ("Idade maior que 18 anos permite cadastro") 
+} else {
+    console.log ("Idade menor que 18 anos, não permite cadastro")
+}
+
+let participantes = 1
+if (participantes >= 100) {
+    console.log ("Limite máximo de participantes atingido")
+} else  {
+    console.log ("Permite cadastro de participantes")
+}
+
+
+
+
+
+
+// let participantes = 99
+// if(participantes >= 100) {
+//     console.log ("Evento Lotado")
+// } else { console.log ("Para realizar o cadastro informe sua idade:")}
+
+// let idade = 19
+// if(idade >=18){
+//     console.log("Em qual data vocÃª gostaria de participar do envento?")
+// } else {console.log("Evento apenas para maiores de 18 anos!")}
+
+// let dataUsuario = "09-01-2022"
+// let hoje = "05-01-2022"
+
+// if (dataUsuario > hoje){
+//     console.log("Data para o evento encontrada, cadastro permitido")
+// } else {console.log("Data nÃ£o encontrada, digite outra data.")}
