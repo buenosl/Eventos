67c36c50c88d62901964840a7ec947e2634c244c
Sandro <sandrobueno@gmail.com>
2022-01-31 11:26:08 -0300

    Limpeza de comentarios

--- evento.js
+++ evento.js
@@ -1,4 +1,3 @@
-// Data Evento
 let dataEvento = 05/02/2022
 if (dataEvento > 06/02/2020 ) {
     console.log ("Data do evento inferior, não permite cadadastro ")
@@ -19,25 +18,3 @@ if (participantes >= 100) {
 } else  {
     console.log ("Permite cadastro de participantes")
 }
-
-
-
-
-
-
-// let participantes = 99
-// if(participantes >= 100) {
-//     console.log ("Evento Lotado")
-// } else { console.log ("Para realizar o cadastro informe sua idade:")}
-
-// let idade = 19
-// if(idade >=18){
-//     console.log("Em qual data vocÃª gostaria de participar do envento?")
-// } else {console.log("Evento apenas para maiores de 18 anos!")}
-
-// let dataUsuario = "09-01-2022"
-// let hoje = "05-01-2022"
-
-// if (dataUsuario > hoje){
-//     console.log("Data para o evento encontrada, cadastro permitido")
-// } else {console.log("Data nÃ£o encontrada, digite outra data.")}
